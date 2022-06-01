e33c4cb85d5be48c45ca751dee312dfaeb565788
scainet <scainet@gmail.com>
2022-06-01 18:00:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from proxypool import ProxyPool, reset_proxy_pool
 
 MOCK_PROXIES = ['192.168.1.120', '192.168.1.130', '192.168.1.140', '192.168.1.150', '192.168.1.160']
 
+async def extraction_task(redis_client, task):
+    for x in range(random.randrange(1, 5)):
+        async with ProxyPool(redis=redis_client, host='www.google.com') as proxy:
+            print(f'{task}:usando proxy {proxy.ip}')
+            if random.random() < 0.3:
+                await proxy.penalize()
+    print(f'{task}:Fin tarea')
+
 
 async def main():
     redis_client = aioredis.from_url("redis://localhost")
@@ -17,5 +25,10 @@ async def main():
                 await proxy.penalize()
 
 
+async def main2():
+    redis_client = aioredis.from_url("redis://localhost")
+    await reset_proxy_pool(redis=redis_client, host='www.google.com', proxies_list=MOCK_PROXIES)
+    await asyncio.gather(*[extraction_task(redis_client, x) for x in range(10)])
+
 if __name__ == '__main__':
-    asyncio.run(main())
+   asyncio.run(main2())
